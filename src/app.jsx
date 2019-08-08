b5a50737d8579c2da09fb56469b74177182fc00b
cragusa21 <collin.ragusa@gmail.com>
2019-08-07 20:01:02 -0500

    Adding app component and changing content

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const app = () => {
+    return (
+        <h1>
+            Welcome to the website of Collin Ragusa
+        </h1>
+    );
+}
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './app';
 
 ReactDOM.render(
-    <div>Hello World</div>,
+    <App />,
     document.getElementById('mainAppRoot')
 );
 
